c2d0935384a747488cc0e2c6975cdf3968f0e39d
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-09-17 14:31:44 +0100

    updated image routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,11 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container">
-        <img className="homeLogo" src="../images/Versus.png" />
+        <img className="homeLogo" src="./images/Versus.png" />
         <h2 className="subtitle is-2">A Premier League comparison site</h2>
       </div>
     </section>
   )
-
 }
 
 export default Home
